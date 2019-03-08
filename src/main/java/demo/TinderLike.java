7f56dac47dc8c2c0781f369e937bddc598c6225f
Guilherme Oliveira <oliveira_guilherme1996@hotmail.com>
2019-03-08 16:22:04 -0300

    Tinder

---
+++ src/main/java/demo/TinderLike.java
@@ -0,0 +1,26 @@
+package demo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TinderLike {
+	private final static String urlTinder = "https://tinder.com/app/recs";
+
+	public static void main(String[] args) throws InterruptedException {
+		//System.setProperty("webdriver.chrome.driver", "C:\\Users\\Guh\\Downloads\\Guilerme\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\guolivei\\Documents\\Projetos\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		try {
+			driver.get(urlTinder);
+			for (int i = 0; i<=100;i++) {
+				Thread.sleep(100);
+				driver.findElement(By.cssSelector(".recsGamepad__button--like")).click();
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		} finally {
+			driver.quit();
+		}
+	}
+}
